0b1e00480654aacd1cf3af9d98ee2e8fbdbb5a21
Fredy Ferney Fuentes Beltran <fredy.fuentesb@cecar.edu.co>
2021-07-31 16:35:21 -0500

    configuracion plantilla adminlte

--- PruebaWeb/App_Start/BundleConfig.cs
+++ PruebaWeb/App_Start/BundleConfig.cs
@@ -8,23 +8,20 @@ namespace PruebaWeb
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery/jquery.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/adminlte").Include(
+                        "~/Scripts/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/Scripts/js/adminlte.min.js"));
+            /*
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
+            */
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/adminlte.min.css",
+                      "~/Content/fontawesome-free/css/all.min.css"));
         }
     }
 }
